a0e5b8461b76d08b4f48f8aef564b2c0737c4765
lokhue <2151013052loc@gmail.com>
2024-12-13 11:26:33 +0700

    chore: dockerize server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import routes from "./routes/index";
-import morgan from "morgan";
+// import morgan from "morgan";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -12,16 +12,18 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 8000;
 
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/api", routes);
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/library"
+  process.env.DATABASE_URL || "mongodb://root:admin123@mongodb:27017/library?authSource=admin"
 );
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
+
+export default app;
